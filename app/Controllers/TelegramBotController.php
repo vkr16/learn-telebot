06845aefbaf4140b1144feb6490e831b14a9f7d7
Fikri Miftah A. <fikri.droid16@gmail.com>
2024-01-15 14:10:06 +0700

    webhook update

--- app/Controllers/TelegramBotController.php
+++ app/Controllers/TelegramBotController.php
@@ -14,7 +14,7 @@ class TelegramBotController extends BaseController
         $message = $update['message']['text'];
 
         $text = "Hi " . $firstname . ", " . $message;
-        $this->sendMessage("chat_id=" . $chat_id . "&text=" . $text);
+        $this->sendMessage($chat_id, $text);
     }
 
     private function readMessage()
@@ -22,9 +22,26 @@ class TelegramBotController extends BaseController
         return json_decode(file_get_contents('php://input'), true);
     }
 
-    private function sendMessage($params)
+    private function sendMessage($chat_id, $text)
     {
-        $url = env('BOT_URL') . env('BOT_TOKEN') . "/sendMessage?" . $params;
-        file_get_contents($url);
+        $url = env('BOT_URL') . env('BOT_TOKEN') . "/sendMessage?";
+
+        $params = [
+            'chat_id' => $chat_id,
+            'text' => $text
+        ];
+
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, env('BOT_URL') . env('BOT_TOKEN') . "/sendMessage?" . http_build_query($params));
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $result;
     }
 }
