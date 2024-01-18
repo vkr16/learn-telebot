f1caffce5bb64705ce3cc98558998d5ec30b6024
Fikri Miftah A. <fikri.droid16@gmail.com>
2024-01-18 10:52:10 +0700

    update webhook

--- app/Controllers/TelegramBotController.php
+++ app/Controllers/TelegramBotController.php
@@ -10,7 +10,7 @@ class TelegramBotController extends BaseController
         '/start' => 'startCommand',
         '/cmd' => 'cmdCommand',
         '/random' =>  'randomCommand',
-        '/appointment' =>  'randomCommand'
+        '/appointment' =>  'appointmentCommand'
     ];
 
     protected $commandDescriptions = [
